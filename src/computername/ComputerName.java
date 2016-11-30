d725efc8f2ae69eac29fbc9d6102fca0839bf6ec
bartvdt <bart.van.der.tang@sprintf.nl>
2016-11-30 19:34:25 +0100

    Update for MacOS

--- src/computername/ComputerName.java
+++ src/computername/ComputerName.java
@@ -1,10 +1,10 @@
 package computername;
 
 import computername.LinuxName;
+import computername.MacOSName;
+import computername.WindowsName;
 
 public class ComputerName {
-
-
 
 public static void main(String[] args) {
 	// TODO Auto-generated method stub
@@ -22,11 +22,27 @@ public static void main(String[] args) {
 	
 	String computerName = "";
 	
+	if (os.indexOf("mac") >= 0)
+		os = "MacOS";
+	else if (os.indexOf("nux") >= 0)
+		os = "Linux";
+	else if (os.indexOf("win") >= 0)
+		os = "Windows";
+	else
+		os = "Unknown";
+	
 	switch (os) {
-	case "linux": 	LinuxName ln = new LinuxName(); 
+	case "MacOS":	MacOSName mn = new MacOSName();
+					computerName = mn.getComputerName();
+					break;
+	case "Linux": 	LinuxName ln = new LinuxName(); 
 					computerName = ln.getComputerName();
 					break;
-	
+	case "Windows": WindowsName wn = new WindowsName();
+					computerName = wn.getComputerName();
+					break;
+	case "Unknown":
+	default:		computerName = "Unknown";
 	}
 	
 	System.out.println("computername: " + computerName);
--- src/computername/WindowsName.java
+++ src/computername/WindowsName.java
@@ -1,9 +1,27 @@
 package computername;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class WindowsName {
 
-	public void getComputerName()
+	public String getComputerName()
 	{
-		System.out.println("TBD");
+		String computerName = "TBD";
+
+		String command = "cat /etc/hostname";
+		try {
+			Process proc = Runtime.getRuntime().exec(command);
+			InputStream stream = proc.getInputStream();
+			Scanner s = new Scanner(stream).useDelimiter("\\A");
+			computerName = s.hasNext() ? s.next() : "";
+	        }
+		catch(IOException e)
+	        {
+	        System.out.println("Unable to execute: " + command );
+	        }
+		
+	    return computerName;
 	}
 }
